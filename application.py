41f1c49805efffad31ce6557f361d14cc9c61efb
bdkautzman <briankautzman@gmail.com>
2024-09-17 15:38:30 +0000

    Specify python module versions, newest releases for some modules caused problems. Add snap for arm arch.

--- application.py
+++ application.py
@@ -105,4 +105,4 @@ def submit(data):
 # Run the app with web sockets capabilities.
 if __name__ == '__main__':
 	app.debug = True
-	socketio.run(app, host='0.0.0.0', port=8080)
+	socketio.run(app, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
